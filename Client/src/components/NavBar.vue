6264859355c03de32837fcd17165565e4daa785b
haachamatcha <re.jaybel@gmail.com>
2024-10-08 14:09:59 -0400

    auto router

--- Client/src/components/NavBar.vue
+++ Client/src/components/NavBar.vue
@@ -1,40 +1,53 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+
+const isOpen = ref(false)
 </script>
 
 <template>
   <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <RouterLink to="/" class="navbar-item">
-          <!--<img src="images/logo.png" alt="logo" width="112" height="28">-->
-          <p class="title is-4 has-text-primary" >MaxFit</p>
-        </RouterLink>  
-        <!--<a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                  <span aria-hidden="true"></span>
-                  <span aria-hidden="true"></span>
-                  <span aria-hidden="true"></span>
-              </a>-->
+    <div class="navbar-brand">
+      <RouterLink to="/" class="navbar-item">
+        <p class="title is-4 has-text-primary">MaxFit</p>
+      </RouterLink>
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        :class="{ 'is-active': isOpen }"
+        @click="isOpen = !isOpen"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div
+      id="navbar"
+      class="navbar-menu is-white"
+      :class="{ 'is-active': isOpen }"
+      @click="isOpen = !isOpen"
+    >
+      <div class="navbar-start">
+        <RouterLink to="/home" class="navbar-item"> Home </RouterLink>
+
+        <RouterLink to="/about" class="navbar-item"> About </RouterLink>
       </div>
 
-      <div id="navbar" class="navbar-menu is-white">
-        <div class="navbar-start">
-            <RouterLink to="/" class="navbar-item"> Home </RouterLink>
-
-            <RouterLink to="/about" class="navbar-item"> About </RouterLink>
-        </div>
-
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="buttons">
-              <RouterLink to="/login" class="has-text-primary">Login</RouterLink>
-              <RouterLink to="/register" class="has-text-primary"> Register </RouterLink>
-            </div>
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <RouterLink to="/login" class="has-text-primary">Login</RouterLink>
+            <RouterLink to="/register" class="has-text-primary"> Register </RouterLink>
           </div>
         </div>
       </div>
-    </nav>
+    </div>
+  </nav>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
